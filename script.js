4cfc0625dc22aa35b5ca57b15d26561f7aa2794e
AmanWorku <amexworku2014@gmail.com>
2022-11-28 21:52:20 +0300

    add javascript  to add, remove and store data locally

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-const titleInput = document.querySelector("#title");
-const authorInput = document.querySelector("#author");
-const button = document.querySelector("#add-btn");
-const bookList = document.querySelector("#book-list")
+/* eslint-disable no-unused-vars */
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const addButton = document.querySelector('.add-btn');
+const bookList = document.querySelector('.books');
 
+let bookArray = [];
 
-button.addEventListener("click", function () {
-    if(titleInput.value == "" && 
-    authorInput.value == ""
-    ){
-        alert("Enter input text");
-    }else {
-        const bookListRow = document.createElement("tr");
-
-        const newTitle = document. createElement("th");
-        newTitle.innerHTML = titleInput.value;
-        bookListRow.appendChild(newTitle);
-
-        const newAuthor = document. createElement("th");
-        newAuthor.innerHTML = authorInput.value;
-        bookListRow.appendChild(newAuthor);
-
-        bookList.appendChild(bookListRow);
+function allBooks() {
+  addButton.addEventListener('click', () => {
+    bookList.innerHTML = '';
+    for (let i = 0; i < bookArray.length; i += 1) {
+      bookList.innerHTML += `
+    <div class="book">
+    <p class="title">${bookArray[i].title}</p>
+    <p class="author">${bookArray[i].author}</p>
+    <button class="remove" onclick="removeBook(${i})">Remove</button><hr>
+    </div>
+    `;
+      title.value = '';
+      author.value = '';
     }
+  });
+}
+
+addButton.addEventListener('click', () => {
+  const book = { title: title.value, author: author.value };
+  bookArray.push(book);
+  allBooks();
+  localStorage.setItem('allBooks', JSON.stringify(bookArray));
 });
+
+window.onload = () => {
+  if (localStorage.getItem('allBooks')) {
+    bookArray = JSON.parse(localStorage.getItem('allBooks'));
+  }
+  allBooks();
+};
+
+function removeBook(item) {
+  bookArray.splice(item, 1);
+  allBooks();
+  localStorage.setItem('allBooks', JSON.stringify(bookArray));
+}
