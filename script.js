58c653925a5f072e2705362fbc2709382d2534f7
AmanWorku <amexworku2014@gmail.com>
2022-11-29 19:16:21 +0300

    add style and edit readme

--- script.js
+++ script.js
@@ -1,54 +1,68 @@
-/* eslint-disable no-unused-vars */
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
-const addButton = document.querySelector('.add-btn');
-const bookList = document.querySelector('.books');
-
-let bookArray = [];
-
-class book {
+/* eslint-disable */
+class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 }
-class UI{
-  static allBooks() {
-  addButton.addEventListener('click', () => {
-    bookList.innerHTML = '';
-    for (let i = 0; i < bookArray.length; i += 1) {
-      bookList.innerHTML += `
-    <div class="book">
-    <p class="title">${bookArray[i].title}</p>
-    <p class="author">${bookArray[i].author}</p>
-    <button class="remove" onclick="UI.removeBook(${i})">Remove</button><hr>
+/* eslint-enable */
+class UI {
+  static displayBooks() {
+    const storedBooks = [];
+    storedBooks.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <div class="remove-tables">
+    <td> "${book.title}" by ${book.author}  <button><a href="#" class="remove-btn">Remove</button></td>
     </div>
     `;
-      title.value = '';
-      author.value = '';
-    }
-  });
-}
-  static removeBook(item) {
-    bookArray.splice(item, 1);
-    UI.allBooks();
-    localStorage.setItem('allBooks', JSON.stringify(bookArray));
+
+    list.appendChild(row);
+    localStorage.setItem('bookList', JSON.stringify(book));
   }
 
+  static deleteBook(ell) {
+    if (ell.classList.contains('remove-btn')) {
+      ell.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
+
+  static getBooks() {
+    return JSON.parse(localStorage.getItem('bookList')) || [];
+  }
 }
 
-addButton.addEventListener('click', () => {
-  const newBook = new book(title.value, author.value);
-  bookArray.push(newBook);
-  UI.allBooks();
-  localStorage.setItem('allBooks', JSON.stringify(bookArray));
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// add books
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // prevent actual submit
+  e.preventDefault();
+  // get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+
+  // instantiate book
+  const book = new Book(title, author);
+
+  // add book to list
+  UI.addBookToList(book);
+
+  // clear fields
+  UI.clearFields();
 });
 
-window.onload = () => {
-  if (localStorage.getItem('allBooks')) {
-    bookArray = JSON.parse(localStorage.getItem('allBooks'));
-  }
-  UI.allBooks();
-};
-
-
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+});
